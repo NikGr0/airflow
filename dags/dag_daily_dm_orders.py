3aa566160abab6db993b5b1f50523e417cf4f06a
Nik <nikitagr01@gmail.com>
2024-09-07 11:40:58 +0500

    dag dm

--- dags/dag_daily_dm_orders.py
+++ dags/dag_daily_dm_orders.py
@@ -3,26 +3,27 @@ from airflow.models import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 
+
 default_args = {
     "owner": "etl_user",
     "depends_on_past": False,
-    "start_date": datetime(2024, 9, 3),
+    "start_date": datetime(2024, 9, 4),
     #"retry_delay": timedelta(minutes=0.1)
 }
 
-dag = DAG('dag_daily_dm_orders', default_args=default_args, schedule_interval='1 * * * *', catchup=True,
+dag = DAG('dag_daily_dm_orders', default_args=default_args, schedule_interval='0 * * * *', catchup=True,
           max_active_tasks=3, max_active_runs=1, tags=["data marts", "dm_orders"])
 
 clear_day = PostgresOperator(
     task_id='clear_day',
-    postgres_conn_id='main_postgresql_connection',
+    postgres_conn_id='main_postgresql_connection_test3',
     sql="""DELETE FROM public.dm_orders  WHERE "buy_date" = '{{ ds }}'::date""",
     dag=dag)
 
 calc_day = PostgresOperator(
     task_id='calc_day',
     postgres_conn_id='main_postgresql_connection_test3',
-    sql="""insert into dm_orders(buy_date, product_name, order_city_name, customer_name, account_number, customer_city_name, total_value, total_costs)
+    sql="""insert into public.dm_orders(buy_date, product_name, order_city_name, customer_name, account_number, customer_city_name, total_value, total_costs)
 select buy_time::date as buy_date, 
 P.product_name as product_name, 
 C.city_name as order_city_name, 
@@ -40,7 +41,7 @@ LEFT JOIN d_customers CU on
 	O.customer_id = CU.id
 LEFT JOIN d_cities as CC on
 	CU.city_id = CC.id
-WHERE buy_date = '{{ ds }}'::date
+WHERE buy_time::date = '{{ ds }}'::date
 group by buy_date, product_name, order_city_name, customer_name, account_number, customer_city_name;""",
     dag=dag)
 
